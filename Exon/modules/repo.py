f0fc224d7468345e4335f76bf172e1e92963bba3
TORZANMOVIE <129737179+TORZANMOVIE@users.noreply.github.com>
2023-06-17 10:52:17 +0530

    Add files via upload

---
+++ Exon/modules/repo.py
@@ -0,0 +1,36 @@
+from platform import python_version as y
+
+from pyrogram import __version__ as z
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import __version__ as o
+from telethon import __version__ as s
+
+from Exon import Abishnoi as pbot
+
+ABISHNOIX = "https://te.legra.ph/file/abfc49a1cc4b5629dc8cd.jpg"
+
+
+@pbot.on_message(filters.command("repo"))
+async def repo(_, message):
+    await message.reply_photo(
+        photo=ABISHNOIX,
+        caption=f"""✨ **ʜᴇʏ {message.from_user.mention},**
+
+**ᴏᴡɴᴇʀ  : [ᴊᴇᴏʟ](https://t.me/jeol_tg)**
+**ᴘʏᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{y()}`
+**ʟɪʙʀᴀʀʏ ᴠᴇʀꜱɪᴏɴ :** `{o}`
+**ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ :** `{s}`
+**ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀꜱɪᴏɴ :** `{z}`
+** ᴇɴᴊᴏʏ**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "•Repo•", url="https://github.com/Itz-JEOL/ROCKY"
+                    ),
+                ]
+            ]
+        ),
+    )
